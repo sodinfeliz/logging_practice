ec072aa642a2408d5660976afb85c43cc4fae709
Elliot Su <sodinfeliz@gmail.com>
2024-04-18 17:42:22 +0800

    [UPDATE] Update `2_hierarchy.py`

--- 2_hierarchy.py
+++ 2_hierarchy.py
@@ -2,18 +2,22 @@ import logging
 
 logging.basicConfig()
 
-
 app_logger = logging.getLogger("app_logger")
+
+f = logging.Formatter(
+    fmt="%(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(f)
+app_logger.addHandler(console_handler)
+
 
 utils_logger = logging.getLogger("app_logger.utils")
 utils_logger.setLevel("DEBUG")
 
-print(app_logger.root.level)
-print(app_logger.root.handlers)
-print(app_logger.level)
-print(app_logger.handlers)
-print(utils_logger.level)
-print(utils_logger.handlers)
+app_logger.propagate = False
 
 
 def main():
